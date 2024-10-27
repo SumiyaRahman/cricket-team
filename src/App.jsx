6caf6aadf027cae93c9c55c2d17f9631c1ff9759
SumiyaRahman <sumiarahman01739@gmail.com>
2024-10-26 23:56:07 -0600

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,37 @@
+import { useState } from "react";
+import Banner from "./Components/Banner";
+import Header from "./Components/Header/Header";
+import Players from "./Components/Players/Players";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Footer from "./Components/Footer";
+
 const App = () => {
+
+  const [claimCoins, setClaimCoins] = useState(0);
+
+  const handleClaimCoins = coin =>{
+    setClaimCoins(previousCoin => previousCoin + coin);
+    toast.success(`Successfully claimed ${coin} coins!`);
+  }
+  
   return (
     <div>
+      <div  className="container mx-auto px-5">
+      <ToastContainer position="top-center" autoClose={2000} hideProgressBar={false} newestOnTop closeOnClick pauseOnFocusLoss draggable pauseOnHover></ToastContainer>
       
+      {/* Header */}
+      <Header claimCoins={claimCoins}></Header>
+
+      {/* banner */}
+      <Banner  handleClaimCoins={handleClaimCoins}></Banner>
+
+      {/* Players */}
+      <Players claimCoins={claimCoins} setClaimCoins={setClaimCoins}></Players>
+
+      </div>
+      {/* Footer */}
+      <Footer></Footer>
     </div>
   );
 };
